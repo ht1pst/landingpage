2515ebe5d6386fd533d68f204476d5d5fc53086c
ht1pst <funtoobadua@gmail.com>
2025-04-09 20:18:42 +0100

    initial commit

---
+++ landing.js
@@ -0,0 +1,9 @@
+// window.addEventListener("scroll", () => {
+//     const features = document.querySelectorAll(".feature");
+//     features.forEach((feature) => {
+//       const rect = feature.getBoundingClientRect();
+//       if (rect.top < window.innerHeight - 100) {
+//         feature.classList.add("show");
+//       }
+//     });
+//   });
